3a97148af64d852d1b1da7f572968f16092db106
Darien Choueiri <darien.choueiri@gmail.com>
2024-02-26 19:08:06 -0500

    added ability to resize grid

--- javascript.js
+++ javascript.js
@@ -1,10 +1,17 @@
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 256; i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
-    grid.appendChild(square);
+function createGrid(size = 16){
+    for (let i = 0; i < size**2; i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+
+        grid.appendChild(square);
+        square.style.height = `${480/size}px`;
+        square.style.width = `${480/size}px`;
+    }
 }
+
+createGrid();
 
 grid.addEventListener('mouseover', (event) => {
     let target = event.target;
@@ -17,5 +24,7 @@ const pixels = document.querySelector('.pixels');
 
 pixels.addEventListener('click', () => {
     let size = prompt("Enter the nuber of squares desired per side (100 max): ");
+    grid.replaceChildren();
+    createGrid(size);
 });
 
